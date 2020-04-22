936f8817ee4f0765130be6aaa44e7fb0cbd63b0a
trung.nguyen <trung.nguyen@kbb.com>
2020-04-22 23:49:06 +0700

    ng generate component for employee and department components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,26 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import {MatInputModule} from '@angular/material/input';
+import { EmployeeComponent } from './employee/employee.component';
+import { ShowEmpComponent } from './employee/show-emp/show-emp.component';
+import { EditEmpComponent } from './employee/edit-emp/edit-emp.component';
+import { AddEmpComponent } from './employee/add-emp/add-emp.component';
+import { DepartmentComponent } from './department/department.component';
+import { ShowDepComponent } from './department/show-dep/show-dep.component';
+import { EditDepComponent } from './department/edit-dep/edit-dep.component';
+import { AddDepComponent } from './department/add-dep/add-dep.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeComponent,
+    ShowEmpComponent,
+    EditEmpComponent,
+    AddEmpComponent,
+    DepartmentComponent,
+    ShowDepComponent,
+    EditDepComponent,
+    AddDepComponent
   ],
   imports: [
     BrowserModule,
